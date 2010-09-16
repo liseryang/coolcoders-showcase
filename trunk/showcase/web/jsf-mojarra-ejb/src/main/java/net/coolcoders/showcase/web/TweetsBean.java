b0beacc192572d1ae49c7b7a5c11467d4fbcd246
andreas.bga@gmail.com <andreas.bga@gmail.com@3d85a60f-d197-fa21-1a62-53c2a78fe69f>
2010-09-16 18:39:57 +0000

    added jsf-icefaces-ejb module

--- trunk/showcase/web/jsf-mojarra-ejb/src/main/java/net/coolcoders/showcase/web/TweetsBean.java
+++ trunk/showcase/web/jsf-mojarra-ejb/src/main/java/net/coolcoders/showcase/web/TweetsBean.java
@@ -5,11 +5,14 @@
 
 package net.coolcoders.showcase.web;
 
+import net.coolcoders.showcase.model.Message;
 import net.coolcoders.showcase.service.MessageServiceBean;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  *
@@ -25,6 +28,11 @@ public class TweetsBean {
     @Inject
     private SessionBean sessionBean;
 
-    
+    public List<Message> getMessages() {
+        List<Message> messages = messageServiceBean.find(new HashMap());
+        return messages;
+    }
+
+
 
 }
