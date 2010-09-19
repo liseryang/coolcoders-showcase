8f69fe306d3b85fd6850fbd1eaf20fc716eff306
p.schneider.manzell <p.schneider.manzell@3d85a60f-d197-fa21-1a62-53c2a78fe69f>
2010-09-19 10:05:04 +0000

    Added dummy user creation to boot strap

--- showcase/web/grails-plain/grails-app/conf/BootStrap.groovy
+++ showcase/web/grails-plain/grails-app/conf/BootStrap.groovy
@@ -26,46 +26,54 @@ class BootStrap {
   }
 
   private void createCategories() {
+    if (!Category.count()) {
+      Category jee5 = new Category(name: "JEE5").save()
+      log.debug("Created category $jee5")
 
-    Category jee5 = new Category(name: "JEE5").save()
-    log.debug("Created category $jee5")
+      Category jee6 = new Category(name: "JEE6").save()
+      log.debug("Created category $jee6")
 
-    Category jee6 = new Category(name: "JEE6").save()
-    log.debug("Created category $jee6")
+      Category grails = new Category(name: "Grails").save()
+      log.debug("Created category $grails")
 
-    Category grails = new Category(name: "Grails").save()
-    log.debug("Created category $grails")
+      Category wicket = new Category(name: "Wicket").save()
+      log.debug("Created category $wicket")
 
-    Category wicket = new Category(name: "Wicket").save()
-    log.debug("Created category $wicket")
-
-    Category gwt = new Category(name: "GWT").save()
-    log.debug("Created category $gwt")
+      Category gwt = new Category(name: "GWT").save()
+      log.debug("Created category $gwt")
+    }
   }
 
 
   private void createUsers() {
-    User pschneidermanzell = new User(username: "pschneider-manzell", password: "test123", email: "pschneider-manzell@coolcoders.net", gender: Gender.MALE).save()
-    log.debug("Created user $pschneidermanzell")
+    if (!User.count()) {
+      User pschneidermanzell = createDummyUser("pschneider-manzell", "Peter Schneider-Manzell", "pschneider-manzell@coolcoders.net", Gender.MALE)
+      User abaumgartner = createDummyUser("abaumgartner", "Andreas Baumgartner", "abaumgartner@coolcoders.net", Gender.MALE)
+      User jmihelko = createDummyUser("jmihelko", "Josip Mihelko", "jmihelko@coolcoders.net", Gender.MALE)
+      User anerlich = createDummyUser("anerlich", "Andreas Baumgartner", "anerlich@coolcoders.net", Gender.MALE)
 
-    User abaumgartner = new User(username: "abaumgartner", password: "test123", email: "abaumgartner@coolcoders.net", gender: Gender.MALE).save()
-    log.debug("Created user $abaumgartner")
+      abaumgartner.addToFollowing(pschneidermanzell)
+      jmihelko.addToFollowing(anerlich)
+      anerlich.addToFollowing(abaumgartner)
 
-    User jmihelko = new User(username: "jmihelko", password: "test123", email: "jmihelko@coolcoders.net", gender: Gender.MALE).save()
-    log.debug("Created user $jmihelko")
-
-    User anerlich = new User(username: "anerlich", password: "test123", email: "anerlich@coolcoders.net", gender: Gender.MALE).save()
-    log.debug("Created user $anerlich")
-
-    abaumgartner.addToFollowing(pschneidermanzell)
-    jmihelko.addToFollowing(anerlich)
-    anerlich.addToFollowing(abaumgartner)
+      for (i in 1..100) {
+        User dummyUser = createDummyUser("user$i", "Dummy User$i", "user$i@coolcoders.net", Gender.MALE)
+        pschneidermanzell.addToFollowing(dummyUser)
+      }
 
 
-    createDummyMessagesForUser(abaumgartner,100)
-    createDummyMessagesForUser(jmihelko,50)
-    createDummyMessagesForUser(anerlich,10)
+      createDummyMessagesForUser(abaumgartner, 100)
+      createDummyMessagesForUser(jmihelko, 50)
+      createDummyMessagesForUser(anerlich, 10)
+    }
 
+  }
+
+
+  User createDummyUser(String username, String fullname, String email, Gender gender) {
+    User dummyUser = new User(username: username, password: "test123", email: email, gender: gender).save()
+    log.debug("Created user $dummyUser")
+    return dummyUser
   }
 
   void createDummyMessagesForUser(User userInstance, int count) {
