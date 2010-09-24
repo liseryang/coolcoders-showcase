d5d14dd2f68dfa2f5d97d6e376b839c8f250fcb3
andreas.bga <andreas.bga@gmail.com>
2010-09-24 23:28:19 +0000

    moved jsf modules into folder jsf

---
+++ showcase/web/jsf/jsf-mojarra-ejb/src/main/java/net/coolcoders/showcase/web/LoginBean.java
@@ -0,0 +1,84 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.coolcoders.showcase.web;
+
+import net.coolcoders.showcase.dao.generic.QueryParameter;
+import net.coolcoders.showcase.model.User;
+import net.coolcoders.showcase.model.User_;
+import net.coolcoders.showcase.service.DbInitBean;
+import net.coolcoders.showcase.service.UserServiceBean;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author andreas
+ */
+@Named
+@RequestScoped
+public class LoginBean {
+
+    @EJB
+    private UserServiceBean userServiceBean;
+
+    @Inject
+    private SessionBean sessionBean;
+
+    @EJB
+    private DbInitBean dbInitBean;
+
+    private String username = null;
+
+    private String password;
+
+    private String message;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String login() {
+        Map<String, Object> restrictions = new HashMap<String, Object>();
+        restrictions.put("username", username);
+        restrictions.put("password", password);
+        
+        User user = userServiceBean.find(QueryParameter.with(User_.username, username).and(User_.password, password));
+        if(user == null) {
+            message = "Login failed!";
+            return null;
+        } else {
+            sessionBean.setCurrentUser(user);
+            return "home";
+        }
+    }
+
+    public String initDb() {
+        dbInitBean.initDb();
+        return null;
+    }
+
+}
---
+++ showcase/web/jsf/jsf-primefaces-ejb-bambo/src/main/java/net/coolcoders/showcase/web/LoginBean.java
@@ -0,0 +1,83 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.coolcoders.showcase.web;
+
+import net.coolcoders.showcase.dao.generic.QueryParameter;
+import net.coolcoders.showcase.model.User;
+import net.coolcoders.showcase.model.User_;
+import net.coolcoders.showcase.service.DbInitBean;
+import net.coolcoders.showcase.service.UserServiceBean;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author andreas
+ */
+@Named
+@RequestScoped
+public class LoginBean {
+
+    @EJB
+    private UserServiceBean userServiceBean;
+
+    @Inject
+    private SessionBean sessionBean;
+
+    @EJB
+    private DbInitBean dbInitBean;
+
+    private String username = null;
+
+    private String password;
+
+    private String message;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String login() {
+        User user = userServiceBean.find(QueryParameter.with(User_.username, username).and(User_.password, password));
+        if(user == null) {
+            message = "Login failed!";
+            return null;
+        } else {
+            sessionBean.setCurrentUser(user);
+            return "home";
+        }
+    }
+
+    public String logout() {
+        sessionBean.setCurrentUser(null);
+        return "login";
+    }
+
+    public String initDb() {
+        dbInitBean.initDb();
+        return null;
+    }
+
+}
