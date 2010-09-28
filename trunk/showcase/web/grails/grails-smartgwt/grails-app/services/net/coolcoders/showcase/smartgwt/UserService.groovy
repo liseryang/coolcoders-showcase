3bed88939f608c7b76cd30e152d30e06becf49d5
josip.mihelko <josip.mihelko@3d85a60f-d197-fa21-1a62-53c2a78fe69f>
2010-09-28 15:26:51 +0000

    ans neue model angepasst

--- trunk/showcase/web/grails/grails-smartgwt/grails-app/services/net/coolcoders/showcase/smartgwt/UserService.groovy
+++ trunk/showcase/web/grails/grails-smartgwt/grails-app/services/net/coolcoders/showcase/smartgwt/UserService.groovy
@@ -1,11 +1,13 @@
 package net.coolcoders.showcase.smartgwt
 
+import net.coolcoders.showcase.AppUser
+
 class UserService {
 
   static transactional = true
 
   def login(String username, String password) {
-    def theUser = User.findByUsername(username)
+    def theUser = AppUser.findByUsername(username)
     if (theUser?.password.equals(password)) {
       return [user: theUser, success: true]
     }
