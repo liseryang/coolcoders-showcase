b033b63d18c1ad08c1249578ed96ae117a6cf6c2
andreas.bga@gmail.com <andreas.bga@gmail.com@3d85a60f-d197-fa21-1a62-53c2a78fe69f>
2010-10-03 22:02:48 +0000

    jsf-primefaces-ejb: added messages for no messages and no friends

--- trunk/showcase/dao/dao-jpa/src/main/java/net/coolcoders/showcase/dao/UserDao.java
+++ trunk/showcase/dao/dao-jpa/src/main/java/net/coolcoders/showcase/dao/UserDao.java
@@ -7,13 +7,17 @@ package net.coolcoders.showcase.dao;
 
 import net.coolcoders.showcase.dao.generic.AbstractGenericDao;
 import net.coolcoders.showcase.model.User;
+import net.coolcoders.showcase.model.User_;
 
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.persistence.criteria.SetJoin;
 
 /**
- *
  * @author andreas
  */
 @Stateless
@@ -24,4 +28,15 @@ public class UserDao extends AbstractGenericDao<User, String> {
         super(User.class);
     }
 
+    public Long count(String userId) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Long> query = cb.createQuery(Long.class);
+        Root<User> root = query.from(User.class);
+        SetJoin<User, User> followingJoin = root.join(User_.following);
+
+        query.select(cb.count(followingJoin));
+        query.where(cb.equal(root.get(User_.id), userId));
+
+        return em.createQuery(query).getSingleResult();
+    }
 }
