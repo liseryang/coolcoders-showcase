d6f0c77118ba26cca892de2543919c598e5a4bed
p.schneider.manzell <p.schneider.manzell@3d85a60f-d197-fa21-1a62-53c2a78fe69f>
2010-10-08 08:02:01 +0000

    Added tests for model classes to showcase-model plugin

---
+++ showcase/web/grails/showcase-model/test/unit/net/coolcoders/showcase/CategoryTests.groovy
@@ -0,0 +1,17 @@
+package net.coolcoders.showcase
+
+import grails.test.GrailsUnitTestCase
+
+class CategoryTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
