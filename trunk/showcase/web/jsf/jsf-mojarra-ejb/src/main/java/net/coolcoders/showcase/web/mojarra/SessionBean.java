310f830af65155bd28a8e20178e84bd0493ca8a5
andreas.bga@gmail.com <andreas.bga@gmail.com@3d85a60f-d197-fa21-1a62-53c2a78fe69f>
2010-10-12 21:06:25 +0000

    jsf-mojarra-ejb: moved backing beans

--- trunk/showcase/web/jsf/jsf-mojarra-ejb/src/main/java/net/coolcoders/showcase/web/mojarra/SessionBean.java
+++ trunk/showcase/web/jsf/jsf-mojarra-ejb/src/main/java/net/coolcoders/showcase/web/mojarra/SessionBean.java
@@ -3,7 +3,7 @@
  * and open the template in the editor.
  */
 
-package net.coolcoders.showcase.web;
+    package net.coolcoders.showcase.web.mojarra;
 
     import net.coolcoders.showcase.model.User;
 
