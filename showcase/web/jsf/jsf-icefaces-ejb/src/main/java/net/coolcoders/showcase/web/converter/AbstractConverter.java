5b12100df6c7ba71e56ec7bb7de25d14e6eccdbe
andreas.bga <andreas.bga@gmail.com>
2010-10-12 21:32:06 +0000

    jsf-icefaces-ejb: fixed profile form

---
+++ showcase/web/jsf/jsf-icefaces-ejb/src/main/java/net/coolcoders/showcase/web/converter/AbstractConverter.java
@@ -0,0 +1,53 @@
+package net.coolcoders.showcase.web.converter;
+
+import net.coolcoders.showcase.service.GenericService;
+
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.servlet.ServletContext;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: andreas
+ * Date: 22.09.2010
+ * Time: 00:47:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class AbstractConverter<T> implements Converter {
+
+    private Class clazz;
+
+    private GenericService genericService;
+
+    public AbstractConverter(Class clazz) {
+        this.clazz = clazz;
+    }
+
+    @Override
+    public Object getAsObject(FacesContext facesContext, UIComponent uiComponent, String s) {
+        if(genericService == null) {
+            BeanManager bm = getBeanManager(facesContext);
+            Bean<GenericService> bean = (Bean<GenericService>) bm.getBeans(GenericService.class).iterator().next();
+            CreationalContext<GenericService> ctx = bm.createCreationalContext(bean);
+            genericService = (GenericService) bm.getReference(bean, GenericService.class, ctx);
+        }
+        return genericService.findById(clazz, s);
+    }
+
+    @Override
+    public String getAsString(FacesContext facesContext, UIComponent uiComponent, Object o) {
+        return (String) o;
+    }
+
+    private BeanManager getBeanManager(FacesContext facesContext)
+    {
+        return (BeanManager)
+              ((ServletContext) facesContext.getExternalContext().getContext())
+                   .getAttribute("javax.enterprise.inject.spi.BeanManager");
+    }
+
+}
