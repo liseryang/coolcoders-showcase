ffb498d5b6ca5534a735e4047b26a32dfca49db2
p.schneider.manzell <p.schneider.manzell@3d85a60f-d197-fa21-1a62-53c2a78fe69f>
2010-10-14 07:50:52 +0000

    Added login help for tk grails showcase

--- showcase/web/zk/zk-grails/grails-app/composers/net/coolcoders/showcase/LoginComposer.groovy
+++ showcase/web/zk/zk-grails/grails-app/composers/net/coolcoders/showcase/LoginComposer.groovy
@@ -8,33 +8,34 @@ import org.zkoss.zk.ui.Executions
 
 class LoginComposer extends GrailsComposer {
 
-    def username
-    def password
-    def login
-    def error
+  def username
+  def password
+  def loginHelpText
+  def login
+  def error
 
-    def afterCompose = { window ->
-        // initialize components here
+  def afterCompose = { window ->
+    loginHelpText.value = "Following users exist by default:\nabaumgartner:test123\nanerlich:test123\njmihelko:test123\npschneider-manzell:test123"
+  }
+
+
+  def onClick_login(MouseEvent me) {
+    def usernameValue = username.value
+    def passwordValue = password.value
+    AppUser userInstance
+    if (usernameValue && passwordValue) {
+      userInstance = AppUser.findByUsernameAndPassword(usernameValue, passwordValue)
+    }
+
+    if (userInstance) {
+      Executions.getCurrent().getSession().putAt("currentUser", userInstance)
+      Executions.getCurrent().sendRedirect("/message")
+    }
+    else {
+      error.value = "Wrong username / password detected!"
+      password.value = ""
     }
 
 
-    def onClick_login(MouseEvent me) {
-      def usernameValue = username.value
-      def passwordValue = password.value
-      AppUser userInstance
-      if(usernameValue && passwordValue) {
-        userInstance = AppUser.findByUsernameAndPassword(usernameValue,passwordValue)
-      }
-
-      if(userInstance) {
-        Executions.getCurrent().getSession().putAt("currentUser",userInstance)
-        Executions.getCurrent().sendRedirect("/message")
-      }
-      else {
-        error.value="Wrong username / password detected!"
-        password.value = ""
-      }
-
-
-    }
+  }
 }
