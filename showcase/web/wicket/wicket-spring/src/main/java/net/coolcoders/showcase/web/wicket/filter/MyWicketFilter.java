417eb0e59b51c970443982cb43da399f5b80ece0
andreas.bga <andreas.bga@gmail.com>
2010-10-26 23:17:57 +0000

    wicket-spring: initial commit

---
+++ showcase/web/wicket/wicket-spring/src/main/java/net/coolcoders/showcase/web/wicket/filter/MyWicketFilter.java
@@ -0,0 +1,14 @@
+package net.coolcoders.showcase.web.wicket.filter;
+
+import org.apache.wicket.protocol.http.WicketFilter;
+
+/**
+ * @author <a href="mailto:andreas@bambo.it">Andreas Baumgartner, andreas@bambo.it</a>
+ *         Date: 24.10.2010
+ *         Time: 17:59:12
+ */
+//@WebFilter(value = "/*",
+//        initParams = {@WebInitParam(name = "applicationClassName",
+//                value = "net.coolcoders.showcase.web.wicket.WicketApplication")})
+public class MyWicketFilter extends WicketFilter {
+}
